1b0c6ae3f318d54aa1f87c3367e59901271ff509
sangwon lee <54252269+leeh8911@users.noreply.github.com>
2020-05-04 10:44:22 +0900

    init master

---
+++ SolvingProblems/Prob1929.h
@@ -0,0 +1,4 @@
+#pragma once
+
+
+int BK_Prob_1929(void);
---
+++ SolvingProblems/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+#include <time.h>
+
+#include "Prob1929.h"
+
+using namespace std;
+
+
+int main(void)
+{
+	BK_Prob_1929();
+
+	return 0;
+}
